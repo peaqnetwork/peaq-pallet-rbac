fcce2b4aaaa08d84e1fbae4d941dfe9a030d9298
Iredia Ebikade <40021650+irediaes@users.noreply.github.com>
2022-08-31 17:57:09 +0100

    feat: added initial pallet structure

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+//! peaq RBAC pallet
+//!
+//! The RBAC pallet allows resolving and management for  role-base access control in a generic manner.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    /// Configure the pallet by specifying the parameters and types on which it depends.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
+
+    // The pallet's runtime storage items.
+    // https://docs.substrate.io/main-docs/build/runtime-storage/
+    #[pallet::storage]
+    #[pallet::getter(fn something)]
+    // Learn more about declaring storage items:
+    // https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
+    pub type Something<T> = StorageValue<_, u32>;
+
+    // Pallets use events to inform users when important changes are made.
+    // https://docs.substrate.io/main-docs/build/events-errors/
+    #[pallet::event]
+    pub enum Event<T: Config> {}
+
+    // Errors inform users that something went wrong.
+    #[pallet::error]
+    pub enum Error<T> {}
+
+    // Dispatchable functions allows users to interact with the pallet and invoke state changes.
+    // These functions materialize as "extrinsics", which are often compared to transactions.
+    // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {}
+}
