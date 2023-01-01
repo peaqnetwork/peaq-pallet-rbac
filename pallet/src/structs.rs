68e30825ad94934f85e3786c75493463c873c7ce
Michael Eberhardt <github@michael-eberhardt.de>
2023-01-01 18:50:22 +0100

    serde needs to be std

--- pallet/src/structs.rs
+++ pallet/src/structs.rs
@@ -2,10 +2,12 @@ use codec::{Decode, Encode};
 use scale_info::TypeInfo;
 use sp_core::RuntimeDebug;
 use sp_std::vec::Vec;
-use serde::{Deserialize, Serialize};
+#[cfg(feature = "std")]
+use serde::{Serialize, Deserialize};
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(
-    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug, Serialize, Deserialize
+    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug //, Serialize, Deserialize
 )]
 pub struct Entity<EntityId> {
     pub id: EntityId,
@@ -13,32 +15,36 @@ pub struct Entity<EntityId> {
     pub enabled: bool,
 }
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(
-    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug, Serialize, Deserialize
+    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug //, Serialize, Deserialize
 )]
 pub struct Role2User<EntityId> {
     pub role: EntityId,
     pub user: EntityId,
 }
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(
-    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug, Serialize, Deserialize
+    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug //, Serialize, Deserialize
 )]
 pub struct Role2Group<EntityId> {
     pub role: EntityId,
     pub group: EntityId,
 }
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(
-    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug, Serialize, Deserialize
+    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug //, Serialize, Deserialize
 )]
 pub struct User2Group<EntityId> {
     pub user: EntityId,
     pub group: EntityId,
 }
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(
-    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug, Serialize, Deserialize
+    Clone, PartialEq, Eq, PartialOrd, Ord, Default, TypeInfo, Decode, Encode, RuntimeDebug //, Serialize, Deserialize
 )]
 pub struct Permission2Role<EntityId> {
     pub permission: EntityId,
