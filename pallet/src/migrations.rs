af29ca4be7ad671a688d41a69542721f6ae23503
talhadaar <muhammadtalhadar@gmail.com>
2024-01-23 14:47:29 +0500

    add further migrations

--- pallet/src/migrations.rs
+++ pallet/src/migrations.rs
@@ -1,15 +1,9 @@
 use super::*;
 
-use crate::rbac::*;
 use frame_support::{
-    dispatch::GetStorageVersion,
-    pallet_prelude::{StorageVersion, ValueQuery},
-    storage_alias,
-    traits::Get,
-    weights::Weight,
-    Blake2_128Concat,
+    dispatch::GetStorageVersion, pallet_prelude::StorageVersion, traits::Get, weights::Weight,
 };
-use structs::Role2User;
+use structs::*;
 
 pub(crate) fn on_runtime_upgrade<T: Config>() -> Weight {
     v1::MigrateToV1x::<T>::on_runtime_upgrade()
@@ -18,6 +12,7 @@ pub(crate) fn on_runtime_upgrade<T: Config>() -> Weight {
 const CURRENT_STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
 
 pub mod v1 {
+
     use super::*;
 
     pub struct MigrateToV1x<T>(sp_std::marker::PhantomData<T>);
@@ -32,6 +27,7 @@ pub mod v1 {
                     current_storage_version
                 );
 
+                // TODO repetitive translation logic with different types, reduce this with a macro
                 // translate all vec to sorted vec
                 Role2UserStore::<T>::translate_values::<Vec<Role2User<T::EntityId>>, _>(
                     |val: Vec<Role2User<T::EntityId>>| {
@@ -41,9 +37,36 @@ pub mod v1 {
                     },
                 );
 
+                // translate all vec to sorted vec
+                Role2GroupStore::<T>::translate_values::<Vec<Role2Group<T::EntityId>>, _>(
+                    |val: Vec<Role2Group<T::EntityId>>| {
+                        let mut sorted_val = val.clone();
+                        sorted_val.sort();
+                        Some(sorted_val)
+                    },
+                );
+
+                // translate all vec to sorted vec
+                User2GroupStore::<T>::translate_values::<Vec<User2Group<T::EntityId>>, _>(
+                    |val: Vec<User2Group<T::EntityId>>| {
+                        let mut sorted_val = val.clone();
+                        sorted_val.sort();
+                        Some(sorted_val)
+                    },
+                );
+
+                // translate all vec to sorted vec
+                Permission2RoleStore::<T>::translate_values::<Vec<Permission2Role<T::EntityId>>, _>(
+                    |val: Vec<Permission2Role<T::EntityId>>| {
+                        let mut sorted_val = val.clone();
+                        sorted_val.sort();
+                        Some(sorted_val)
+                    },
+                );
+
                 // upgrade current_storage_version
                 log::info!(
-                    "Update current storage version of migration to {:?}",
+                    "Initialize current storage version of migration to {:?}",
                     current_storage_version
                 );
                 current_storage_version.put::<Pallet<T>>();
