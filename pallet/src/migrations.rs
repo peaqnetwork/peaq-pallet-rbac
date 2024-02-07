f571b613365a22e33eddbc1c68035d7ddd13e7dd
talhadaar <muhammadtalhadar@gmail.com>
2024-02-07 15:42:47 +0500

    removed unnecesary module declaration

--- pallet/src/migrations.rs
+++ pallet/src/migrations.rs
@@ -7,81 +7,75 @@ use sp_std::vec::Vec;
 use structs::*;
 
 pub(crate) fn on_runtime_upgrade<T: Config>() -> Weight {
-    migrations::MigrateToV1x::<T>::on_runtime_upgrade()
+    MigrateToV1x::<T>::on_runtime_upgrade()
 }
 
-pub mod migrations {
+// This migration sort all values in some StorageMaps allowing them to be binary searchable
+// As there was no storage version set before, we set it now to 0
+pub struct MigrateToV1x<T>(sp_std::marker::PhantomData<T>);
 
-    use super::*;
+impl<T: Config> MigrateToV1x<T> {
+    pub fn on_runtime_upgrade() -> Weight {
+        let target_storage_version: StorageVersion = Pallet::<T>::current_storage_version();
+        let on_chain_storage_version: StorageVersion = Pallet::<T>::on_chain_storage_version();
 
-    // This migration sort all values in some StorageMaps allowing them to be binary searchable
-    // As there was no storage version set before, we set it now to 0
-    pub struct MigrateToV1x<T>(sp_std::marker::PhantomData<T>);
+        let mut weight: u64 = 0;
+        if on_chain_storage_version < target_storage_version {
+            log::info!(
+                "Pallet RBAC: Migration from onchain version {:?} to current version {:?}",
+                on_chain_storage_version,
+                target_storage_version,
+            );
 
-    impl<T: Config> MigrateToV1x<T> {
-        pub fn on_runtime_upgrade() -> Weight {
-            let target_storage_version: StorageVersion = Pallet::<T>::current_storage_version();
-            let on_chain_storage_version: StorageVersion = Pallet::<T>::on_chain_storage_version();
+            // TODO repetitive translation logic with different types, reduce this with a macro
+            // translate all vec to sorted vec
+            Role2UserStore::<T>::translate_values::<Vec<Role2User<T::EntityId>>, _>(
+                |val: Vec<Role2User<T::EntityId>>| {
+                    let mut sorted_val = val.clone();
+                    sorted_val.sort();
+                    weight += 1;
+                    Some(sorted_val)
+                },
+            );
 
+            // translate all vec to sorted vec
+            Role2GroupStore::<T>::translate_values::<Vec<Role2Group<T::EntityId>>, _>(
+                |val: Vec<Role2Group<T::EntityId>>| {
+                    let mut sorted_val = val.clone();
+                    sorted_val.sort();
+                    weight += 1;
+                    Some(sorted_val)
+                },
+            );
 
-            let mut weight: u64 = 0;
-            if on_chain_storage_version < target_storage_version {
-                log::info!(
-                    "Pallet RBAC: Migration from onchain version {:?} to current version {:?}",
-                    on_chain_storage_version,
-                    target_storage_version,
-                );
+            // translate all vec to sorted vec
+            User2GroupStore::<T>::translate_values::<Vec<User2Group<T::EntityId>>, _>(
+                |val: Vec<User2Group<T::EntityId>>| {
+                    let mut sorted_val = val.clone();
+                    sorted_val.sort();
+                    weight += 1;
+                    Some(sorted_val)
+                },
+            );
 
-                // TODO repetitive translation logic with different types, reduce this with a macro
-                // translate all vec to sorted vec
-                Role2UserStore::<T>::translate_values::<Vec<Role2User<T::EntityId>>, _>(
-                    |val: Vec<Role2User<T::EntityId>>| {
-                        let mut sorted_val = val.clone();
-                        sorted_val.sort();
-                        weight +=1;
-                        Some(sorted_val)
-                    },
-                );
+            // translate all vec to sorted vec
+            Permission2RoleStore::<T>::translate_values::<Vec<Permission2Role<T::EntityId>>, _>(
+                |val: Vec<Permission2Role<T::EntityId>>| {
+                    let mut sorted_val = val.clone();
+                    sorted_val.sort();
+                    weight += 1;
+                    Some(sorted_val)
+                },
+            );
 
-                // translate all vec to sorted vec
-                Role2GroupStore::<T>::translate_values::<Vec<Role2Group<T::EntityId>>, _>(
-                    |val: Vec<Role2Group<T::EntityId>>| {
-                        let mut sorted_val = val.clone();
-                        sorted_val.sort();
-                        weight +=1;
-                        Some(sorted_val)
-                    },
-                );
-
-                // translate all vec to sorted vec
-                User2GroupStore::<T>::translate_values::<Vec<User2Group<T::EntityId>>, _>(
-                    |val: Vec<User2Group<T::EntityId>>| {
-                        let mut sorted_val = val.clone();
-                        sorted_val.sort();
-                        weight +=1;
-                        Some(sorted_val)
-                    },
-                );
-
-                // translate all vec to sorted vec
-                Permission2RoleStore::<T>::translate_values::<Vec<Permission2Role<T::EntityId>>, _>(
-                    |val: Vec<Permission2Role<T::EntityId>>| {
-                        let mut sorted_val = val.clone();
-                        sorted_val.sort();
-                        weight +=1;
-                        Some(sorted_val)
-                    },
-                );
-
-                // upgrade current_storage_version
-                log::info!(
-                    "Pallet RBAC: Setting storage version to {:?}",
-                    target_storage_version
-                );
-                target_storage_version.put::<Pallet<T>>();
-            }
-            log::info!("Weight calculated: {:?}", weight);
-            T::DbWeight::get().reads_writes(weight, weight)
+            // upgrade current_storage_version
+            log::info!(
+                "Pallet RBAC: Setting storage version to {:?}",
+                target_storage_version
+            );
+            target_storage_version.put::<Pallet<T>>();
         }
+        log::info!("Weight calculated: {:?}", weight);
+        T::DbWeight::get().reads_writes(weight, weight)
     }
 }
