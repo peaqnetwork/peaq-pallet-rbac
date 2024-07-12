f67052231aae082c7d9d333ab867dfecd917ce71
Jay Pan <pan@peaq.io>
2024-07-12 05:11:32 -0700

    Update v1.7.2

--- pallet/src/mock.rs
+++ pallet/src/mock.rs
@@ -54,6 +54,7 @@ impl system::Config for Test {
     type SS58Prefix = SS58Prefix;
     type OnSetCode = ();
     type MaxConsumers = frame_support::traits::ConstU32<16>;
+	type RuntimeTask = ();
 }
 
 parameter_types! {
@@ -73,9 +74,10 @@ impl pallet_balances::Config for Test {
     type AccountStore = System;
     type WeightInfo = ();
     type FreezeIdentifier = ();
-    type MaxHolds = ();
+    // type MaxHolds = ();
     type MaxFreezes = ();
     type RuntimeHoldReason = ();
+	type RuntimeFreezeReason = ();
 }
 
 parameter_types! {
